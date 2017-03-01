60e29287f88b372b0cd661758632f8fa02a63d73
Sheludiackow <leshiy.ru@bk.ru>
2017-03-02 02:12:53 +0500

    Добавили кнопки в окно. Применили BorderLayout

--- src/Form.java
+++ src/Form.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Programmer on 02.03.2017.
@@ -8,6 +9,16 @@ public class Form extends JFrame {
         setTitle("Test Window");  // Текст в шапке окна
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Чтобы программа полностью перестовала работать при нажатии крестика
         setBounds(600,200,400,400);   // Сдвоеный метод, для проставления размеров окна и его положения
+        JButton[] jbs = new JButton [5];  // Создали кнопки
+            for (int i = 0; i < jbs.length; i++) {
+                jbs [i] = new JButton("#"+i);
+        }
+        setLayout(new BorderLayout()); // Как кнопки будут выглядеть на экране
+        add(jbs[0],BorderLayout.EAST);
+        add(jbs[1],BorderLayout.WEST);
+        add(jbs[2],BorderLayout.NORTH);
+        add(jbs[3],BorderLayout.SOUTH);
+        add(jbs[4],BorderLayout.CENTER);
         setVisible(true); // Для того чтоб окно появилось
     }
 }
