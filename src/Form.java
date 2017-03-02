3e5b9f888632bf8b03091e2e5caf3c01974c3b3a
Sheludiackow <leshiy.ru@bk.ru>
2017-03-02 17:00:31 +0500

    Создали кнопки при помощи BoxLayout

--- src/Form.java
+++ src/Form.java
@@ -9,16 +9,18 @@ public class Form extends JFrame {
         setTitle("Test Window");  // Текст в шапке окна
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Чтобы программа полностью перестовала работать при нажатии крестика
         setBounds(600,200,400,400);   // Сдвоеный метод, для проставления размеров окна и его положения
+        setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS)); // Как кнопки будут выглядеть на экране
         JButton[] jbs = new JButton [5];  // Создали кнопки
             for (int i = 0; i < jbs.length; i++) {
                 jbs [i] = new JButton("#"+i);
+                jbs [i].setAlignmentY(Component.CENTER_ALIGNMENT); // Вызываем Layout и выравниваем по Y
         }
-        setLayout(new BorderLayout()); // Как кнопки будут выглядеть на экране
-        add(jbs[0],BorderLayout.EAST);
-        add(jbs[1],BorderLayout.WEST);
-        add(jbs[2],BorderLayout.NORTH);
-        add(jbs[3],BorderLayout.SOUTH);
-        add(jbs[4],BorderLayout.CENTER);
+
+        add(jbs[0]);
+        add(jbs[1]);
+        add(jbs[2]);
+        add(jbs[3]);
+        add(jbs[4]);
         setVisible(true); // Для того чтоб окно появилось
     }
 }
